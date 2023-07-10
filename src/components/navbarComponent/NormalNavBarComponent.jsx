06f2d32850ef629c29887e7cff25704b745a7598
sokpheng001 <115383587+sopi001@users.noreply.github.com>
2023-07-10 23:50:00 +0700

    Search feature ✨✨✨❤️❤️❤️

--- src/components/navbarComponent/NormalNavBarComponent.jsx
+++ src/components/navbarComponent/NormalNavBarComponent.jsx
@@ -1,13 +1,27 @@
 'use client'
 import React, {useEffect, useState} from "react";
-import {Button, IconButton, MobileNav, Navbar, Typography,Input} from "@material-tailwind/react";
+import {Button, IconButton, Input, MobileNav, Navbar, Typography} from "@material-tailwind/react";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {Formik} from "formik";
+import {useFormik} from "formik";
 import * as yup from "yup"
 
 
+const  localhost_search_for_video_api = `http://localhost:8080/api/v3/videos/search_query=;`
 
+// const search_query = async (title)=>{
+//     const query = await fetch(localhost_search_for_video_api + title);
+//     return await query.json();
+// }
+
+
+//API
+
+//     search input
+//     yup
+const yupValidators = yup.object({
+    search:yup.string().required("Content is needed.")
+})
 
 export default function NormalNavBarComponent() {
   const [openNav, setOpenNav] = useState(false);
@@ -29,104 +43,127 @@ export default function NormalNavBarComponent() {
         }
     }
 
-
   useEffect(() => {
     window.addEventListener("resize", () => window.innerWidth >= 960 && setOpenNav(false));
   }, []);
-// 
-  const navList = (
-    <ul className="justify-center mb-4 mt-2  flex flex-row flex-wrap gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-          {border("/","Home")}
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-          {border("/flps","FLPs")}
-      </Typography>
-      {/*  videos*/}
-        <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-        >
-            {border("/videos","Videos")}
-        </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-          {border("/blocks","Blocks")}
-      </Typography>
-      {/*  */}
-        <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-        >
-            {border("/production","Production")}
-        </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-          {border("/faqs","FAQs")}
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-          {border("/about-us","About us")}
-      </Typography>
-    </ul>
-  );
-//  end of nav list
-//     search input
-//     yup
-    const yupValidators = yup.object({
-        search:yup.string()
-    })
+//
+
+
+    // formik
+    const  formik = useFormik({
+        initialValues:{
+            search:"",
+        },
+        validationSchema:yupValidators,
+        // onSubmit: async (values)=>{
+        // //     to do
+        //     const fetching = await search_query(values.search);
+        //     alert(JSON.stringify(fetching.data[0].videoTitle))
+        // },
+        onSubmit: (values)=>{
+            //     to do
+            alert(JSON.stringify(values))
+        }
+    });
     // search
     const searchInput = (
-        <section className={"flex justify-center"}>
-            {/* search button */}
-            <div className="relative flex w-full gap-2 md:w-max">
+         // search button
+        <section className={"justify-center flex"}>
+            <form onSubmit={formik.handleSubmit} className="relative flex w-full gap-2 md:w-max">
                 <Input
-                    type="search"
+                    type="text"
                     label="Search here..."
-                    className="pr-10 border-gray-300 focus:border-t-0 text-black shadow-none focus:shadow-none min-w-[500px]"
+                    className="pr-10 border-gray-300 focus:border-t-0 text-black shadow-none focus:shadow-none  md:min-w-[500px]"
                     color={"gray"}
-                    value={""}
+                    name={"search"}
+                    id={"search"}
+                    value={formik.values.search}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    helperText={formik.touched.search && formik.errors.search}
                 />
-                <Button size="sm" className="!absolute right-1 top-1 rounded bg-gray-300 text-black shadow-none hover:shadow-none" >
-                    <Link href={"/videos"}>
-                        Search
-                    </Link>
+                <Button type={"submit"} size="sm" className="!absolute right-1 top-1 rounded bg-gray-300 text-black shadow-none hover:shadow-none" >
+                    Search
                 </Button>
-            </div>
-            {/*  end of search*/}
+            </form>
         </section>
+    // {/*  end of search*/}
     )
+  //   end of search
+  const navList = (
+      <Navbar shadow={false}>
+          <ul className="justify-center mb-4 mt-2  flex flex-row flex-wrap gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
+              <Typography
+                  as="li"
+                  variant="small"
+                  color="blue-gray"
+                  className="p-1 font-normal"
+              >
+                  {border("/","Home")}
+              </Typography>
+              <Typography
+                  as="li"
+                  variant="small"
+                  color="blue-gray"
+                  className="p-1 font-normal"
+              >
+                  {border("/flps","FLPs")}
+              </Typography>
+              {/*  videos*/}
+              <Typography
+                  as="li"
+                  variant="small"
+                  color="blue-gray"
+                  className="p-1 font-normal"
+              >
+                  {border("/videos","Videos")}
+              </Typography>
+              <Typography
+                  as="li"
+                  variant="small"
+                  color="blue-gray"
+                  className="p-1 font-normal"
+              >
+                  {border("/blocks","Blocks")}
+              </Typography>
+              {/*  */}
+              <Typography
+                  as="li"
+                  variant="small"
+                  color="blue-gray"
+                  className="p-1 font-normal"
+              >
+                  {border("/production","Production")}
+              </Typography>
+              <Typography
+                  as="li"
+                  variant="small"
+                  color="blue-gray"
+                  className="p-1 font-normal"
+              >
+                  {border("/faqs","FAQs")}
+              </Typography>
+              <Typography
+                  as="li"
+                  variant="small"
+                  color="blue-gray"
+                  className="p-1 font-normal"
+              >
+                  {border("/about-us","About us")}
+              </Typography>
+          </ul>
+          {/*search input*/}
+          <div>
+              {searchInput}
+          </div>
+      </Navbar>
+  );
+//  end of nav list
+
 // path name
 
 const pathName = usePathname();
-if(pathName==="/login" || pathName.includes("/admin")){
+if(pathName==="/login" || pathName.includes("/admin") || pathName.includes("sign-in") || pathName.includes("sign-up") ){
   return null;
 }
 // 
@@ -147,17 +184,22 @@ if(pathName==="/login" || pathName.includes("/admin")){
         <div className="hidden lg:block">
             {/*{navList}*/}
             {searchInput}
+
         </div>
         {/*  */}
         <div className="hidden gap-2 lg:flex">
           <Button variant="text" size="sm" color="gray" className={"hover:shadow-gray-50 hover:bg-gray-200"}>
-            Sign In
+              <Link href={"/sign-in"}>
+                  Sign In
+              </Link>
           </Button>
           <Button
             className="bg-gray-300 shadow-none hover:shadow-gray-50 text-black"
             size="sm"
           >
-            Sign Up
+              <Link href={"/sign-up"}>
+                  Sign Up
+              </Link>
           </Button>
 
         </div>
